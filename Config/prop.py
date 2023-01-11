eb1b1f15affe7325de695ea908955799ccd449b4
hz157 <horizon221@foxmail.com>
2023-01-11 17:48:58 +0800

    prop env config

---
+++ Config/prop.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023.
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Project: MIDEP
+# @FileName: Config/prop.py
+# @Author：Ryan Zhang    (https://github.com/hz157)
+# @DateTime: 10/1/2023 下午11:14
+
+"""Mysql Config"""
+Mysql_echo = False  # ORM转话SQL语句打印
+
