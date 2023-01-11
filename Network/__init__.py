f07102dc21c271010f9b33ce46648105acd31fd0
hz157 <horizon221@foxmail.com>
2023-01-11 17:50:47 +0800

    Network init

---
+++ Network/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2023.
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Project: MIDEP
+# @FileName: Network/__init__.py
+# @Author：Ryan Zhang    (https://github.com/hz157)
+# @DateTime: 10/1/2023 下午11:44
+from Network.Files import *
+from Network.Sina import *
+
