11774199047f2d0db5aa93d0f5cf46c6b8b54b27
ryan.zhang <horizon221@foxmail.com>
2023-03-19 11:31:24 +0800

    地图服务API

---
+++ api/baidu.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2023.
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Project: mist
+# @FileName: baidu
+# @Author：Ryan Zhang    (https://github.com/hz157)
+# @DateTime: 3/3/2023 上午10:31
+
+"""
+    百度地图开放平台
+"""
+
+
+import requests
+from urllib import parse
+from config import config
+import hashlib
+import json
+
+from unit.coordinate import bd09_to_wgs84
+
+# 百度地图开放平台 URL (修改./config/config.py)
+host = config.BAIDU_API_HOST
+# 百度地图开放平台访问密钥 (修改./config/config.py)
+ak = config.BAIDU_API_AK
+
+
+def calculateSn(queryStr: str):
+    """
+        sn签名计算
+        https://lbsyun.baidu.com/index.php?title=lbscloud/api/appendix
+    Args:
+        queryStr: 请求地址
+
+    Returns: sn签名
+    """
+    # 对queryStr进行转码，safe内的保留字符不转换
+    encodedStr = parse.quote(queryStr, safe="/:=&?#+!$,;'@()*[]")
+    # 在最后直接追加上yoursk
+    rawStr = encodedStr + ak
+    return hashlib.md5(parse.quote_plus(rawStr).encode()).hexdigest()
+
+
+def place_v2_search(query: str, region: str = "全国"):
+    """
+        地点检索V2.0 请求发起
+        https://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-placeapi#service-page-anchor-1-3
+    Args:
+        query: 检索关键字
+        region: 行政区划区域. Defaults to "全国".
+
+    Returns: dict(地点名称，经纬度)
+    """
+    # 地点检索V2.0 URL地址
+    apiURL = f'/place/v2/search?query={query}&region={region}&output=json&ak={ak}'
+    # SN 计算与链接
+    apiURL = apiURL + '&sn=' + calculateSn(apiURL)
+    # 发起网络请求
+    response = json.loads(requests.get(host + apiURL).text)
+    # 请求结果
+    result = []
+    # 遍历返回数据
+    for i in response['results']:
+        # 构造列表（地点名称，经纬度，街景地图ID)
+        try:
+            result.append(
+                {"name": i['name'],
+                 'bd-09': {'lng': i['location']['lng'], 'lat': i['location']['lat']},
+                 'street_id': i['street_id'] if 'street_id' in i.keys() else None})
+        except Exception as e:
+            print(e)
+            return None
+    if result:
+        return result[0]    # 默认返回第一个，相关地较高
+    return None
+
+#
+# a = place_v2_search('厦门市湖里区嘉禾路468号SM城市广场F1')
+# print(a)
+# wgs84 = bd09_to_wgs84(a['bd-09']['lng'], a['bd-09']['lat'])
+# a['wgs-84'] = {'lng': wgs84[0], 'lat': wgs84[1]}
+# print(a)
---
+++ api/tencent.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @ Project: new_mist
+# @ File: tencent
+# @ Time: 6/3/2023 上午8:44
+# @ Author: hz157
+# @ Github: https://github.com/hz157
+
+"""
+    腾讯位置服务
+"""
+
+
+import json
+import requests
+import hashlib
+
+from config import config
+from unit.coordinate import gcj02_to_wgs84
+
+# 腾讯地图开放平台 URL (修改./config/config.py)
+host = config.TENCENT_API_HOST
+# 腾讯地图开放平台访问密钥 (修改./config/config.py)
+key = config.TENCENT_API_KEY
+# 腾讯地图开放平台sig计算密钥 (修改./config/config.py)
+sk = config.TENCENT_API_SK
+
+
+def calculateSig(queryStr: str):
+    """
+        sig签名计算
+        https://lbs.qq.com/faq/serverFaq/webServiceKey
+    Args:
+        queryStr: 请求地址
+
+    Returns: md5 加密后的sig签名
+
+    """
+    return hashlib.md5(queryStr.encode('utf-8')).hexdigest()
+
+
+def geocoder(query: str):
+    """
+        地址解析（地址转坐标） 请求发起
+        https://lbs.qq.com/service/webService/webServiceGuide/webServiceGeocoder
+    Args:
+        query: 检索关键字
+
+    Returns:
+
+    """
+    # 地点解析 URL地址
+    apiURL = f'/ws/geocoder/v1/?address={query}&key={key}'
+    # Sig 计算与链接
+    apiURL = apiURL + '&sig=' + calculateSig(apiURL + sk)
+    # 发起网络请求
+    response = json.loads(requests.get(host + apiURL).text)
+    if response['status'] != 0:
+        return None
+    # 请求结果
+    result = {"name": response['result']['title'],
+              'gcj-02': {'lng': response['result']['location']['lng'], 'lat': response['result']['location']['lat']}}
+    return result
+
+#
+# a = geocoder('厦门市思明南路422号')
+# print(a)
+# wgs84 = gcj02_to_wgs84(a['gcj-02']['lng'], a['gcj-02']['lat'])
+# a['wgs-84'] = {'lng': wgs84[0],'lat': wgs84[1]}
+# print(a)
