f4bb46bb14cecff2f40bae833e01b1facb42cab9
ryan.zhang <horizon221@foxmail.com>
2023-03-20 19:45:50 +0800

    更改列名

--- main.py
+++ main.py
@@ -32,8 +32,10 @@ def read_weibo(path, fileType):
 if __name__ == '__main__':
     star_time = datetime.now()
     current = 0
+    # fields = ['text', 'sentence', 'create_at', 'ner_time', 'ner_gpe', 'ner_fac', 'stand_time', 'stand_time_status',
+    #           'stand_loc', 'stand_loc_status', 'lng-bd09', 'lat-bd09', 'lng-wgs84', 'lat-wgs84', 'street_id']  # 列名
     fields = ['text', 'sentence', 'create_at', 'ner_time', 'ner_gpe', 'ner_fac', 'stand_time', 'stand_time_status',
-              'stand_loc', 'stand_loc_status', 'lng-bd09', 'lat-bd09', 'lng-wgs84', 'lat-wgs84', 'street_id']  # 列名
+              'stand_loc', 'stand_loc_status', 'loc_bd09', 'loc_wgs84', 'street_id']  # 列名
     write_csv(config.SAVE_PATH, fields)  # 写标签列   验证csv可能会导致mid错乱，弃用csv
     # write_excel(config.SAVE_PATH, fields)   # 写标签列
     data = read_weibo(config.ORIGINAL_PATH, fileType="excel")  # 读取数据库导出的EXCEL数据
